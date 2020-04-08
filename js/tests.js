5d8c63a3b551d152b54e0bb75a88a0433dc2753c
masterful-array-3850 <crgammage@gmail.com>
2020-04-08 15:03:05 +0000

    Automatically backed up by Learn

--- js/tests.js
+++ js/tests.js
@@ -8,7 +8,7 @@ if (index.name === "Joe") {
 
 
 
-if (index.height === "+index.height") {
+if (index.height === "74") {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
